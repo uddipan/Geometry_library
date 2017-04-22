8a0ff4ea03699b0181cd0ea621afbd01d88ca0c0
uddipan mukherjee <uddipanm@gmail.com>
2017-04-22 10:22:49 -0700

    clean commit

---
+++ geometry_library/geometry_library/include/Geomheader.h
@@ -0,0 +1,31 @@
+//
+//  Geomheader.h
+//  GeomLib
+//
+//  Created by uddipan mukherjee on 4/9/12.
+//  Copyright (c) 2012 University of California, Irvine. All rights reserved.
+//
+
+#ifndef GeomLib_Geomheader_h
+#define GeomLib_Geomheader_h
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <string>
+#include <cmath>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GLUT/GLUT.h>
+#include <OpenGL/OpenGL.h>
+
+
+#include "Vec2.h"
+#include "Vec3.h"
+
+#endif
---
+++ geometry_library/geometry_library/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  GeomLib
+//
+//  Created by uddipan mukherjee on 4/9/12.
+//  Copyright (c) 2012 University of California, Irvine. All rights reserved.
+//
+// Implementing geometry library
+/*
+ 1. Area of a polygon implementation - AreaPoly.cpp
+ */
+
+#include "include/GeomHeader.h"
+int Width = 600, Height = 500;
+extern void display();
+
+int main(int argc, char** argv) {
+  // display routine required for testing
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE);
+  glutInitWindowSize(Width, Height);
+  glutInitWindowPosition(100, 100);
+  glutCreateWindow("Test Window");
+  // glutDisplayFunc(display);
+  glutMainLoop();
+
+  return 0;
+}
